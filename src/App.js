53f054a9c7c31352f4d2ade270d0dfbf3bddca04
dkomarov <dkomarov@hawk.iit.edu>
2020-07-10 14:43:37 -0500

    use BrowserRouter from react-router
    to handle routing inside root 'App' component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Mario Plan</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
