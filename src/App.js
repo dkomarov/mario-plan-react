4a1789367a93d91abd08fdb1ef826e51db5577bd
dkomarov <dkomarov@hawk.iit.edu>
2020-07-10 21:55:17 -0500

    change App from function to class
    resolves browser error by rendering the BrowserRouter React element

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar/>
-        <h1>MarioPlan</h1>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+            <Switch>
+              <Route path='/' component={Dashboard} />
+            </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
