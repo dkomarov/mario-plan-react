9418ed2fb6c3ca366f3ee188d41bfb739f44fec5
dkomarov <dkomarov@hawk.iit.edu>
2020-07-12 12:52:44 -0500

    import thunk for createStore

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
