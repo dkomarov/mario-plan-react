31156652b4143fe899289e044f531ddb5e122254
dkomarov <dkomarov@hawk.iit.edu>
2020-07-12 12:55:31 -0500

    send projects prop to their components
    sample projects now accessible via the project list & summary, from the project reducer

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,11 +4,13 @@ import ProjectList from '../projects/ProjectList'
 
 class Dashboard extends Component {
   render() {
+    // console.log(this.props)
+    const { projects } = this.props
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-              <ProjectList />
+              <ProjectList projects={projects} />
           </div>
             <div className="col s12 m5 offset-m1">
               <Notifications />
