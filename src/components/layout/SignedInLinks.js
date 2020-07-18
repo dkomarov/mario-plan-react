25a18e592fa8418c6356d046e06342a922d84324
dkomarov <dkomarov@hawk.iit.edu>
2020-07-18 13:18:53 -0500

    rename file

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { signOut } from '../../store/actions/authAction'
+import { signOut } from '../../store/actions/authActions'
 
 const SignedInLinks = (props) => {
   return (
